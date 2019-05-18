a80fb79d6123a230f66983277e371fca60040520
Maksim Ivanov <satansdeer@gmail.com>
2019-05-18 21:45:58 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Query } from "react-apollo";
+import { gql } from "apollo-boost";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
+        padding: "50px"
+      }}
+    >
+      <Query
+        query={gql`
+          {
+            photos {
+              url
+              author
+              comments {
+                author
+                text
+              }
+            }
+          }
+        `}
+      >
+        {({ loading, error, data }) => {
+          if (loading) return <p>Loading...</p>;
+          if (error) return <p>Error :(</p>;
+
+          return data.photos.map(photo => (
+            <div
+              key={photo.url}
+              style={{
+                border: "1px solid #e6e6e6",
+                borderRadius: "3px",
+                marginBottom: "50px",
+                backgroundColor: "#fff"
+              }}
+            >
+              <div
+                style={{ padding: "16px", fontSize: "14px", fontWeight: 800 }}
+              >
+                {photo.author}
+              </div>
+              <img
+                style={{ width: "400px" }}
+                src={photo.url}
+                alt={photo.author}
+              />
+              <div style={{padding: 16}}>
+              {photo.comments.map(comment => (
+                <div style={{ display: 'flex', marginBottom: 6 }} key={comment.text}>
+                  <div style={{ fontWeight: 800, marginRight: "10px" }}>
+                    {comment.author}
+                  </div>
+                  {comment.text}
+                </div>
+              ))}
+            </div>
+            </div>
+          ));
+        }}
+      </Query>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: "https://kittengram-server.glitch.me/"
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
