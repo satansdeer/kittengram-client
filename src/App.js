c517abb56e0631d8b4dd744ea1247900722e7f32
Maksim Ivanov <satansdeer@gmail.com>
2019-05-20 21:38:42 +0200

    Add styles

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import React from "react";
 import { Query } from "react-apollo";
 import { gql } from "apollo-boost";
 import "./App.css";
+import {
+  Container,
+  Header,
+  Card,
+  Image,
+  Footer,
+  Comment,
+  Author
+} from "./Components";
 
 function App() {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        flexDirection: "column",
-        alignItems: "center",
-        padding: "50px"
-      }}
-    >
+    <Container>
       <Query
         query={gql`
           {
@@ -33,40 +34,22 @@ function App() {
           if (error) return <p>Error :(</p>;
 
           return data.photos.map(photo => (
-            <div
-              key={photo.url}
-              style={{
-                border: "1px solid #e6e6e6",
-                borderRadius: "3px",
-                marginBottom: "50px",
-                backgroundColor: "#fff"
-              }}
-            >
-              <div
-                style={{ padding: "16px", fontSize: "14px", fontWeight: 800 }}
-              >
-                {photo.author}
-              </div>
-              <img
-                style={{ width: "400px" }}
-                src={photo.url}
-                alt={photo.author}
-              />
-              <div style={{padding: 16}}>
-              {photo.comments.map(comment => (
-                <div style={{ display: 'flex', marginBottom: 6 }} key={comment.text}>
-                  <div style={{ fontWeight: 800, marginRight: "10px" }}>
-                    {comment.author}
-                  </div>
-                  {comment.text}
-                </div>
-              ))}
-            </div>
-            </div>
+            <Card key={photo.url}>
+              <Header>{photo.author}</Header>
+              <Image src={photo.url} alt={photo.author} />
+              <Footer>
+                {photo.comments.map(comment => (
+                  <Comment key={comment.text}>
+                    <Author>{comment.author}</Author>
+                    {comment.text}
+                  </Comment>
+                ))}
+              </Footer>
+            </Card>
           ));
         }}
       </Query>
-    </div>
+    </Container>
   );
 }
 
---
+++ src/Components/index.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  padding: 50px;
+`;
+
+export const Card = styled.div`
+  border: 1px solid #e6e6e6;
+  border-radius: 3px;
+  margin-bottom: 50px;
+  background-color: #fff;
+`;
+
+export const Header = styled.div`
+  padding: 16px;
+  font-size: 14px;
+  font-weight: 800;
+`;
+
+export const Image = styled.img`
+  width: 400px;
+`;
+
+export const Footer = styled.div`
+  padding: 16px;
+`;
+
+export const Comment = styled.div`
+  display: flex;
+  margin-bottom: 6;
+`
+
+export const Author = styled.div`
+  font-weight: 800;
+  margin-right: 10px;
+`
